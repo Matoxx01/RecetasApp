afce763d589988caaf4e8bbe6082d1336a259feb
Matoxx01 <matiasbarrios2010@gmail.com>
2024-09-05 13:29:08 -0400

    HolaMundo

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RecetasApp',
+  webDir: 'dist'
+};
+
+export default config;
